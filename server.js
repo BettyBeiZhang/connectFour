68e49537264b8becd279cd36b547e9094a761f16
bei.zhang <bei.zhang@jolokia.com>
2018-07-04 15:48:04 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 //simple express server to serve the app on Heroku
+// const express = require('express');
+// const app = express();
+
+// app.use(express.static (__dirname + '/dist'));
+
+// // force redirect to HTTPS
+// const forceSSL = function(){
+//     return function (req,res, next) {
+//         if(req.headers['x-forwarded-proto'] !== 'https'){
+//             return res.redirect (
+//                 ['https://' + req.host('Host'), req.url].join('')
+//             );
+//         }
+//         next();
+//     }
+// }
+
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static (__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/my_app'));
 
-// force redirect to HTTPS
-const forceSSL = function(){
-    return function (req,res, next) {
-        if(req.headers['x-forwarded-proto'] !== 'https'){
-            return res.redirect (
-                ['https://' + req.host('Host'), req.url].join('')
-            );
-        }
-        next();
-    }
-}
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/my_app/index.html'));
+});
 
 
-app.use(forceSSL());
+
+// app.use(forceSSL());
 app.listen (process.env.PORT || 8080); 
